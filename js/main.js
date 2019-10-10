cfc76ed64ab287b7eac9c100030271453dcfb4f0
Hasibur Rahman <hasibonline2@gmail.com>
2019-10-10 14:05:36 +0600

    slider

--- js/main.js
+++ js/main.js
@@ -24,8 +24,11 @@
   function heroSlider() {
     $("#hero-carousel").owlCarousel({
       loop: true,
-      margin: 10,
+      margin: 0,
       // nav:true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      // autoplayHoverPause: true,
       items: 1
     });
   }
